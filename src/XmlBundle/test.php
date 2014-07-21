81666470edcfb850f5ec5792718ab6b4adbe3157
Desperado <m.masukevich@dengionline.com>
2014-07-21 11:41:48 +0400

    Change structure of a bundle

--- src/XmlBundle/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$data = ['foo' => 'bar'];
-
-$xmlPrepare = new \Desperado\XmlBundle\Model\XmlPrepare;
-$xmlGenerator = new Desperado\XmlBundle\Model\XmlGenerator;
-
-echo $xmlGenerator->generateFromArray($xmlPrepare->prepareArrayBeforeToXmlConvert($data));
